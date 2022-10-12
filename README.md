69412ec0466e5e1cb3649d92b548d4ac75b45880
Chris He <hwachris@amazon.com>
2022-10-12 10:45:09 +0800

    Add function files & update README.

--- README.md
+++ README.md
@@ -1,11 +1,36 @@
-## My Project
+## lambda-runtime-update-helper
 
-TODO: Fill this README out!
+### Getting started
 
-Be sure to:
+This project is created to help for update AWS Lambda function runtime by batch. The function will update Lambda runtime in $LATEST version.
 
-* Change the title in this README
-* Edit your repository description on GitHub
+### Pre-request
+
+- [ ] [Complete basic AWS CLI Configuration with Admin right](https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-quickstart.html)
+- [ ] boto3 is required for the project
+
+### How to run
+
+Update region and runtime info in [app.py](handler/app.py)
+
+```
+# List runtime needs to be update here
+# 'nodejs'|'nodejs4.3'|'nodejs6.10'|'nodejs8.10'|'nodejs10.x'|'nodejs12.x'|'nodejs14.x'|'nodejs16.x'|'java8'|'java8.al2'|'java11'|'python2.7'|'python3.6'|'python3.7'|'python3.8'|'python3.9'|'dotnetcore1.0'|'dotnetcore2.0'|'dotnetcore2.1'|'dotnetcore3.1'|'dotnet6'|'nodejs4.3-edge'|'go1.x'|'ruby2.5'|'ruby2.7'|'provided'|'provided.al2'
+sourceRuntimeList = ('nodejs12.x','nodejs14.x')
+
+# Target update runtime
+targetRuntime = 'nodejs16.x'
+
+# Client config
+client_config = Config(
+    region_name='us-east-1'
+)
+```
+
+Run the function
+```
+python handler/app.py
+```
 
 ## Security
 
---
+++ handler/app.py
@@ -0,0 +1,51 @@
+import boto3
+from botocore.config import Config
+
+# List runtime needs to be updated here
+sourceRuntimeList = ('nodejs14.x',)
+
+# Target update runtime
+targetRuntime = 'nodejs16.x'
+
+# Client config
+client_config = Config(
+    region_name='us-east-1'
+)
+
+
+def runtime_update_handler():
+    client = boto3.client('lambda', config=client_config)
+
+    response = client.list_functions(MaxItems=30)
+
+    function_list_for_update = []
+
+    while True:
+        function_list = response.get('Functions')
+        print('Get ' + str(len(function_list)) + ' functions in response.')
+
+        it = iter(function_list)
+        for func in it:
+            if func.get('Runtime') in sourceRuntimeList:
+                function_list_for_update.append(func.get('FunctionName'))
+
+        print('Find ' + str(len(function_list_for_update)) + ' function(s) will be updated.')
+
+        if len(function_list_for_update) > 0:
+            it_for_update = iter(function_list_for_update)
+
+            for funcName in it_for_update:
+                print('Update function:' + funcName)
+                client.update_function_configuration(FunctionName=funcName, Runtime=targetRuntime)
+
+            function_list_for_update.clear()
+
+        if response.get('NextMarker') is not None:
+            response = client.list_functions(Marker=response.get('NextMarker'), MaxItems=30)
+        else:
+            break
+
+    print('Update completed!')
+
+
+runtime_update_handler()
